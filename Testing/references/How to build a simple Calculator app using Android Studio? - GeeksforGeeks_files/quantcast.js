51a27ae0af4960a16427d1994507aa72e47d5843
kadylaptop <gyrbuilder45@outlook.com>
2021-10-29 16:16:45 -0700

    some more stuff, simple proj website to test from

---
+++ Testing/references/How to build a simple Calculator app using Android Studio? - GeeksforGeeks_files/quantcast.js
@@ -0,0 +1,13 @@
+window._qevents = window._qevents || [];
+(function() {
+var elem = document.createElement('script');
+elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+elem.async = true;
+elem.type = "text/javascript";
+var scpt = document.getElementsByTagName('script')[0];
+scpt.parentNode.insertBefore(elem, scpt);
+})();
+window._qevents.push({
+qacct:"p-54Nt-1NAaEEe0",
+uid:"__INSERT_EMAIL_HERE__"
+});
