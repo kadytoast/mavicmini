6b4e1699d36c11d35e15e369388c3577dc70c4a7
KadyD <gyrbuilder45@outlook.com>
2021-10-30 15:55:46 -0700

    importsdk testing, currently not working, builds but crashes

--- testing/ImportSDK/app/src/main/java/com/mavicmini/importsdk/MApplication.java
+++ testing/ImportSDK/app/src/main/java/com/mavicmini/importsdk/MApplication.java
@@ -10,6 +10,7 @@ public class MApplication extends Application {
     @Override
     protected void attachBaseContext(Context paramContext) {
         super.attachBaseContext(paramContext);
+        // install dji sdk, must be done before using any sdk stuff
         Helper.install(MApplication.this);
     }
 }
