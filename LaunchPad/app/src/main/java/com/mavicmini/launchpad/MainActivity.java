7577555ba0e628d33b368e7253abe5bafd32728d
kadylaptop <gyrbuilder45@outlook.com>
2021-11-02 13:15:02 -0700

    more looking:

--- LaunchPad/app/src/main/java/com/mavicmini/launchpad/MainActivity.java
+++ LaunchPad/app/src/main/java/com/mavicmini/launchpad/MainActivity.java
@@ -3,12 +3,24 @@ package com.mavicmini.launchpad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+/** API 30 **/
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set dji perms
+        checkAndRequestPermissions();
+
+        // when done, confirm its prep
+        TextView hellotxt = findViewById(R.id.textHello);
+        hellotxt.setText(R.string.doneperms);
+    }
+
+    public void checkAndRequestPermissions () {
     }
 }
--- testing/ImportSDK/app/src/main/java/com/mavicmini/importsdk/MainActivity.java
+++ testing/ImportSDK/app/src/main/java/com/mavicmini/importsdk/MainActivity.java
@@ -63,9 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
         // When the compile and target version is higher than 22, please request the
         // following permission at runtime to ensure the SDK works well.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            checkAndRequestPermissions();
-        }
+        checkAndRequestPermissions();
 
 
         //Initialize DJI SDK Manager
