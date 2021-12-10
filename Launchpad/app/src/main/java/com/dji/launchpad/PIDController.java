d907a1e6cc157620fa22f35d92c94549ea6409f3
kadylaptop <gyrbuilder45@outlook.com>
2021-12-10 12:10:09 -0800

    control loop class and basics pseudo added

---
+++ Launchpad/app/src/main/java/com/dji/launchpad/PIDController.java
@@ -0,0 +1,23 @@
+package com.dji.launchpad;
+
+public class PIDController {
+    /*
+    error_prior = 0
+    integral_prior = 0
+    KP = Some value you need to come up (see tuning section below)
+    KI = Some value you need to come up (see tuning section below)
+    KD = Some value you need to come up (see tuning section below)
+    bias = 0 (see below)
+
+    while(1) {
+    error = desired_value – actual_value
+    integral = integral_prior + error * iteration_time
+    derivative = (error – error_prior) / iteration_time
+    output = KP*error + KI*integral + KD*derivative + bias
+    error_prior = error
+    integral_prior = integral
+    sleep(iteration_time)
+    }
+
+    */
+}
