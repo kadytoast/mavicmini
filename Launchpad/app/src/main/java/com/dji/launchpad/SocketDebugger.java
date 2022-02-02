9b4b09a8682e47d7e84191b0cdaa085348c6a080
kadylaptop <gyrbuilder45@outlook.com>
2022-02-02 10:44:17 -0800

    basics for node.js debug server

--- Launchpad/app/src/main/java/com/dji/launchpad/SocketDebugger.java
+++ Launchpad/app/src/main/java/com/dji/launchpad/SocketDebugger.java
@@ -2,8 +2,12 @@ package com.dji.launchpad;
 
 /**
  * class for interacting with custom socketio based wireless debug client
+ *
+ * create class instance, then after app launch, take input of server ip and port, then connect
+ * and start sending log info
  */
 public class SocketDebugger {
+
 
 
 }
