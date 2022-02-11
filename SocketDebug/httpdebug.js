381c671f9c0fbb4ba4e29fe3591637a6a98b574d
kadydesk <gyrbuilder45@outlook.com>
2022-02-10 23:33:29 -0800

    fixed post http server, communication now functional, next will be testing debug output in field -- v0.1.5

--- SocketDebug/httpdebug.js
+++ SocketDebug/httpdebug.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const ip = require('ip')
 const port = 8080
 
-app.get('/debug', (req, res) => {
+app.post('/debug', (req, res) => {
     res.send("msg logged")
     console.log("body: ")
     console.log(req.body)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on ${ip.address}:${port}`)
+    console.log(`Example app listening on ${ip.address()}:${port}`)
 })
